8f0ea25e423a8202763ef544e1ed3a9a6263b903
Nikhil Janshali <nikhil.janshali@gmail.com>
2023-08-27 12:11:59 +0530

    chanages: header changes

---
+++ src/app/component/flip-logo/flip-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flip-logo',
+  templateUrl: './flip-logo.component.html',
+  styleUrls: ['./flip-logo.component.css']
+})
+export class FlipLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
