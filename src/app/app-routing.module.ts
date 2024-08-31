a58a451745eb902df4c39b88a613e6f765ee7d81
Nikhil Janshali <nikhil.janshali@gmail.com>
2024-08-31 22:14:31 +0530

    publish changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { ArchiveComponent } from './components/archive/archive.component';
 
 const routes: Routes = [
 
-  {path: ':language?',      component: HomeComponent},
-  {path: ':language?/proyectos',   component: ArchiveComponent},
+  // { path: ':language?', component: HomeComponent },
+  // { path: ':language?/proyectos', component: ArchiveComponent },
+  { path: '', component: HomeComponent },
 
   // {path: 'profile'        ,   component: ProfileComponent         , canActivate: [AuthGuard]},
   // {path: 'users'          ,   component: UsersComponent           , canActivate: [AuthGuard]},
   // {path: 'register-user'  ,   component: RegisterUserComponent    , canActivate: [AuthGuard]},
 
-  {path: '**', pathMatch: 'full', redirectTo: '/'},
+  { path: '**', pathMatch: 'full', redirectTo: '/' },
 
 ];
 
